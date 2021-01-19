0db5ab6c6c060435db25f97569c519ff6e976a82
Stephen Molyneaux <WimbledonLabs@github.com>
2021-01-18 16:01:05 -0800

    Simplify post process script

--- pipeline/post_process.py
+++ pipeline/post_process.py
@@ -1,4 +1,49 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.6
+"""
+This script is run by Medusa (https://github.com/pymedusa/Medusa) as a
+post-process script (https://github.com/pymedusa/Medusa/wiki/Post-Processing).
+
+This script just records the information provided by medusa into a
+configurable directory:
+    argv[0]: File-path to Script
+    argv[1]: Final full path to the episode file
+    argv[2]: Original full path of the episode file
+    argv[3]: Show indexer ID
+    argv[4]: Season number
+    argv[5]: Episode number
+    argv[6]: Episode Air Date
+
+Previously, I (SHM) configured this to do much more (transcoding, backup,
+and metadata conversion), but that had problems. The biggest problem was
+that the post processing had bugs. In particular, certain directories
+were being removed unexpectedly. I believe it's not the post processing
+script that's doing this, but making the script more simple will show
+that more obviously.
+
+Medusa also has terrible, terrible feedback when post processing scripts fail.
+Like, none. Super duper sucks. We need to do something _really_ simple here to
+have any hope of feedback. We _can_ scrape the logs and re-run this if something
+goes wrong, but it's nice to not have to!
+
+Another benefit of structuring the pipeline this way is that it's easier
+to debug. Rather than reproducing the command line arguments passed by
+medusa to this script, I can use the output file from this script to
+feed the next script in the pipeline. The JSON output from this script
+is easier to understand at a glance.
+
+I typically try to make things configurable. For this pipeline, I think
+that has hurt more than it helped. It meant that _this_ script would
+decide on output locations for files. These output locations don't need
+to be configurable, and making the configurable (and required!) made
+these scripts more annoying to develop. So, against what I typically do,
+the invocations of these scripts will be simplified, and these scripts
+will become less configurable.
+
+There is a downside to this approach. It means I need to schedule
+something to search through the output directory of this script to kick
+off the next steps of the pipeline. Previously, I could rely on medusa
+to kick things off.
+"""
 import json
 import logging
 import sys
@@ -6,62 +51,40 @@ import subprocess
 import os
 from pathlib import Path
 
-def run_cmd(args):
-    logging.debug(f"Running: {args}")
-    try:
-        subprocess.run(args, stderr=subprocess.STDOUT, check=True)
-    except subprocess.CalledProcessError as e:
-        logging.error(e)
-        sys.exit(e.returncode)
+from common import Config, VideoInfo
 
-script_dir = Path(__file__).absolute().parent
+script_dir = Path(__file__).resolve().parent
 
-# Keep secrets out of git :)
-with open(script_dir.joinpath("post_process_config.json").as_posix()) as f:
-    config = json.loads(f.read())
+config_file = script_dir.joinpath("post_process_config.json")
+config = Config.from_json(config_file.read_text())
 
-log_location = config.get('log_location', 'post_process.log')
+log_location = (
+    Path(config.base_log_location) / Path(__file__).with_suffix(".log").parts[-1]
+)
+
+print(f"Logging to {log_location}", file=sys.stderr)
+log_location.parent.mkdir(parents=True, exist_ok=True)
+
 logging.basicConfig(filename=log_location, level=logging.DEBUG)
 
-try:
-    assert "b2_bucket" in config
+_, video_path, source_path, tvdbid, season, episode, air_date = sys.argv
 
-    nfo_to_json = script_dir.joinpath("nfo_to_json.py").as_posix()
-    video_to_mpd = script_dir.joinpath("video_to_mpd.py").as_posix()
+video_info = VideoInfo(
+    video_path=video_path,
+    source_path=source_path,
+    tvdbid=int(tvdbid),
+    season=int(season),
+    episode=int(episode),
+    air_date=air_date,
+)
 
-    # We don't control the CLI, so it's not useful to use argparse
-    _, video_path, source_path, tvdbid, season, episode, air_date = sys.argv
-    season = int(season)
-    episode = int(episode)
+post_process_queue_dir = Path(config.post_process_queue)
+post_process_queue_dir.mkdir(parents=True, exist_ok=True)
 
-    # Convert the episode metadata from .nfo to .json
+info_path = post_process_queue_dir.joinpath(
+    f"{video_info.tvdbid}_S{video_info.season}E{video_info.episode}"
+)
 
-    # Input metadata
-    nfo_metadata = Path(video_path).with_suffix(".nfo")
+logging.info(f"Writing {video_info} to {info_path}")
 
-    # Output metadata
-    json_metadata = Path(video_path).with_suffix(".json")
-
-    nfo_to_json_args = [
-        nfo_to_json,
-        nfo_metadata,
-        "--output",
-        json_metadata,
-        "--source",
-        Path(source_path).stem,
-    ]
-    run_cmd(nfo_to_json_args)
-
-    # Convert the video and upload to b2
-    run_cmd([
-        video_to_mpd,
-        "--input",
-        video_path,
-        "--b2-bucket",
-        config["b2_bucket"],
-        "--b2-dir",
-        f"content/{tvdbid}/S{season:02}E{episode:02}",
-    ])
-
-except Exception as e:
-    logging.error(e)
+info_path.write_text(video_info.to_json() + "\n")
